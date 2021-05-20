f1e3bab7a55e925e90a3170fe2dce730069b112f
Wiktor Tylman <wiktor7245@o2.pl>
2021-05-20 18:58:26 +0200

    Initial commit

---
+++ src/com/wiktyl/Ocena.java
@@ -0,0 +1,78 @@
+package com.wiktyl;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+
+import net.sourceforge.jFuzzyLogic.Gpr;
+
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+import javax.swing.*;
+
+
+public class Ocena {
+	public static Double score = 0d;
+
+	public static void main(String[] args) throws Exception {
+
+		setUpFrame();
+		
+		setFclFile();
+
+		// Load from 'FCL' file
+		String fileName = "./ocena.fcl";
+		FIS fis = FIS.load(fileName, true);
+		if( fis == null ) { 
+			System.err.println("Nie moge zaladowc pliku: '" + fileName + "'");
+			return;
+		}
+
+		// Pokazuje reguly
+		FunctionBlock functionBlock = fis.getFunctionBlock(null);
+	        JFuzzyChart.get().chart(functionBlock);	
+
+		// Ustawia wejscia
+		fis.setVariable("koszty", 5000);
+		fis.setVariable("trudnosc", 8);
+		fis.setVariable("dlugosc", 6);
+
+		// Wylicza zbiory rozmyte
+		fis.evaluate();
+
+		// Ustawia wyjscia
+		Variable projekt = functionBlock.getVariable("projekt");
+
+
+
+		// Pokazuje wykres zmiennych wyjsciowych
+
+		JFuzzyChart.get().chart(projekt, projekt.getDefuzzifier(), true);
+		
+		// Drukuje reguly
+		System.out.println("fis");
+		System.out.println(fis);
+		System.out.println("Wynik liczbowy:" + fis.getVariable("projekt").getValue());
+		System.out.println("Metoda:" + fis.getVariable("projekt").getName());
+		System.out.println("Projekt:" + fis.getVariable("projekt"));
+		System.out.println("% przynależności do zbioru \"dobry\":" + projekt.getMembership("dobry")*100 + "%");
+
+		score = fis.getVariable("projekt").getValue();
+	}
+
+	private static void setFclFile() {
+	}
+
+	private static void setUpFrame() {
+		JFrame frame = new JFrame("InputForm");
+		frame.setContentPane(new InputForm().rootPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setSize(500,500);
+		frame.setVisible(true);
+		frame.setTitle("System oceny projektów");
+	}
+
+}
