563fac464743edd3083775dc3d3220a10285b412
Wiktor Tylman <korek44@gmail.com>
2021-06-05 23:20:31 +0200

    refactored code and made some changes

--- src/com/wiktyl/main.py
+++ src/com/wiktyl/main.py
@@ -30,7 +30,7 @@ import pytesseract
 import io
 from PIL import Image
 from wand.image import Image as wi
-import gc
+# import gc
 import json
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
@@ -44,66 +44,29 @@ def get_text_from_image(pdf_path):
     pdfImg=pdf.convert('jpeg')
     imgBlobs=[]
     extracted_text = []
-    # try:
-    #     for img in pdfImg.sequence:
-    #         page=wi(image=img)
-    #         imgBlobs.append(page.make_blob('jpeg'))
-    #         # for i in range(0,5):
-    #         #     [gc.collect() for i in range(0,10)]
-    #
-    #     for imgBlob in imgBlobs:
-    #         im=Image.open(io.BytesIO(imgBlob))
-    #         text=pytesseract.image_to_string(im,lang='pol')
-    #         text = text.replace(r"\n", " ")
-    #         extracted_text.append(text)
-    #         # for i in range(0,5):
-    #         #     [gc.collect() for i in range(0,10)]
-    #     print("ext")
-    #     print(extracted_text)
-    #     print("extfin")
-    #     for i in extracted_text:
-    #         print(i)
-    #         if "Koszt:" in i:
-    #             print("XD")
-    #             print(i)
-    #             json_data['koszt'] = i
-    #         # json_data.append(i)
-    #     # json_data.append(extracted_text)
-    #     with open(pdf_path + '.json','w',encoding='utf-8') as outfile:
-    #         json.dump(json_data,outfile,ensure_ascii=False)
-    #     # return (''.join([i.replace("\n"," ")for i in extracted_text]))
-    #     # [gc.collect() for i in range(0,10)]
-    # finally:
-    #     # [gc.collect() for i in range(0,10)]
-    #     img.destroy()
 
     for img in pdfImg.sequence:
         page=wi(image=img)
         imgBlobs.append(page.make_blob('jpeg'))
-        # for i in range(0,5):
-        #     [gc.collect() for i in range(0,10)]
 
     for imgBlob in imgBlobs:
         im=Image.open(io.BytesIO(imgBlob))
         text=pytesseract.image_to_string(im,lang='pol')
         text = text.replace(r"\n", " ") #this might be bad
         extracted_text = text.split("\n")
-        # for i in range(0,5):
-        #     [gc.collect() for i in range(0,10)]
+
     print(extracted_text)
+
     for i in extracted_text:
         print(i)
-        if "Koszt:" in i:
-            print("XD")
-            print(i)
+        if "Koszt: " in i:
             json_data['koszt'] = i[i.index("Koszt: ") + len("Koszt: "):]
-            # s1[s1.index(s2) + len(s2):]
-        # json_data.append(i)
-    # json_data.append(extracted_text)
+        if "Czas trwania: " in i:
+            json_data['czas_trwania'] = i[i.index("Czas trwania: ") + len("Czas trwania: "):]
+        if "Trudność projektu: " in i:
+            json_data['trudnosc_projektu'] = i[i.index("Trudność projektu: ") + len("Trudność projektu: "):]
+
     with open(pdf_path + '.json','w',encoding='utf-8') as outfile:
         json.dump(json_data,outfile,ensure_ascii=False)
-    # return (''.join([i.replace("\n"," ")for i in extracted_text]))
-    # [gc.collect() for i in range(0,10)]
-# print(get_text_from_image("file.pdf"))
-# get_text_from_image("file.pdf")
+
 get_text_from_image("test_project.pdf")
